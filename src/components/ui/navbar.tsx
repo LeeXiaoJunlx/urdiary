6c754e6ef8e17751f9815ff4530a0e3453b94367
IfalEXZ <ifaltampan69@gmail.com>
2025-01-12 15:35:56 +0800

    feat: integrate dialog component into Navbar for 'Tentang' section with detailed description

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,7 +1,9 @@
-import { Button } from './button'
-import { ModeToggle } from '../mode-toggle'
+import { Button } from '@/components/ui/button'
+import { ModeToggle } from '@/components/mode-toggle'
 import Link from 'next/link'
 import { Github } from 'lucide-react'
+import { Dialog, DialogContent, DialogTitle, DialogDescription, DialogTrigger } from './dialog'
+
 
 export const Navbar = () => {
     return (
@@ -9,7 +11,20 @@ export const Navbar = () => {
         <div className="container mx-auto px-4 py-3 flex justify-between items-center">
           <div className="font-bold text-lg">Keluh Kesah</div>
           <div className="flex items-center gap-4">
-            <Button variant="neutral">Tentang</Button>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="neutral">Tentang</Button>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogTitle>Tentang Keluh Kesah</DialogTitle>
+                <DialogDescription>
+                  Keluh Kesah adalah adalah tempat bagi siapa saja untuk mencurahkan isi hati dan berbagi pengalaman. Platform ini dibuat dengan tujuan untuk mengurangi angka stress di Indonesia. (Anjay)
+                </DialogDescription>
+                <DialogDescription>
+                  Dibuat oleh <Link href="https://ifal.me" className="text-main hover:text-black">Ifal Fahri A</Link>
+                </DialogDescription>
+            </DialogContent>
+          </Dialog>
             <Link href="https://github.com/ifalfahri/keluhkesah"><Button variant="neutral" className="w-10 h-10"><Github /></Button></Link>
             <ModeToggle />
           </div>
