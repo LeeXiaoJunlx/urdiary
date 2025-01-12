87e6fd394f2c8055b36ea58ad8ee6ae34b8ab492
IfalEXZ <ifaltampan69@gmail.com>
2025-01-12 14:50:50 +0800

    feat: add Label and Switch components; update Home page layout with new UI elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,25 @@
-'use client';
+"use client";
 
-import { useState, useEffect } from 'react';
-import { Button } from '@/components/ui/button';
-import { KeluhCard } from '@/components/keluh-card';
-import { KeluhAdd } from '@/components/keluh-add';
-import { getPosts } from '@/lib/storage';
-import { MessageSquarePlus } from 'lucide-react';
-import { KeluhPost } from './types';
-import { ModeToggle } from '@/components/mode-toggle';
-import { SkeletonCard } from '@/components/skeleton-card';
-import { cn } from '@/lib/utils';
-import { InteractiveGridPattern } from '@/components/ui/interactive-grid-pattern';
-
+import { KeluhAdd } from "@/components/keluh-add";
+import { KeluhCard } from "@/components/keluh-card";
+import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+import { InteractiveGridPattern } from "@/components/ui/interactive-grid-pattern";
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
+import { getPosts } from "@/lib/storage";
+import { cn } from "@/lib/utils";
+import { MessageSquarePlus } from "lucide-react";
+import { useEffect, useState } from "react";
+import { KeluhPost } from "./types";
 
 export default function Home() {
   const [posts, setPosts] = useState<KeluhPost[]>([]);
   const [isNewPostOpen, setIsNewPostOpen] = useState(false);
-  const [loading, setLoading] = useState(true);
 
   const loadPosts = async () => {
-    setLoading(true);
     const posts = await getPosts();
     setPosts(posts);
-    setLoading(false);
   };
 
   useEffect(() => {
@@ -33,24 +30,28 @@ export default function Home() {
     <main className="relative min-h-screen bg-gray-50 dark:bg-zinc-900">
       <InteractiveGridPattern
         className={cn(
-          "[mask-image:radial-gradient(700px_circle_at_center,white,transparent)] ",
+          "[mask-image:radial-gradient(700px_circle_at_center,white,transparent)] "
         )}
         width={30}
         height={30}
         squares={[80, 80]}
         squaresClassName="hover:fill-main"
       />
-      <nav className="relative z-10 border-b-4 border-border bg-bg">
+      <nav className="fixed top-0 right-0 w-full z-50 border-b-4 border-border bg-bg">
         <div className="container mx-auto px-4 py-3 flex justify-between items-center">
-          <div className="font-bold text-lg">
-            Keluh Kesah
-          </div>
+          <div className="font-bold text-lg">Keluh Kesah</div>
           <div className="flex items-center gap-4">
             <ModeToggle />
           </div>
         </div>
       </nav>
-      <div className="container mx-auto px-4 py-8">
+      <div className="container mx-auto px-4 py-8 pt-24">
+        <div className="flex items-center space-x-2 justify-center mb-6">
+          <Switch id="wibu-mode" className="z-10" />
+          <Label htmlFor="wibu-mode" className="z-10">
+            Wibu Mode
+          </Label>
+        </div>
         <div className="flex flex-col items-center mb-12">
           <h1 className="relative z-10 text-4xl font-black text-center mb-4">
             Keluh Kesah
@@ -59,7 +60,7 @@ export default function Home() {
             Silahkan berkeluh kesah di sini.
           </p>
           <Button
-            className='relative z-10'
+            className="relative z-10"
             size="lg"
             onClick={() => setIsNewPostOpen(true)}
           >
@@ -68,25 +69,15 @@ export default function Home() {
           </Button>
         </div>
 
-        {loading ? (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 max-w-7xl mx-auto">
-            {[...Array(6)].map((_, index) => (
-              <SkeletonCard key={index} />
-            ))}
-          </div>
-        ) : (
+        
           <div className="relative z-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 max-w-7xl mx-auto">
             {posts.map((post) => (
               <KeluhCard key={post.id} post={post} onUpdate={loadPosts} />
             ))}
           </div>
-        )}
+        
 
-        {posts.length === 0 && !loading && (
-          <div className="text-center text-muted-foreground mt-8">
-            Belum ada yang ngeluh nih. Yuk mulai ngeluh!
-          </div>
-        )}
+        
 
         <KeluhAdd
           open={isNewPostOpen}
